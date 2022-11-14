a7f994ccb81117da86dccbabc254bca0c46bc428
kkkmj <wjsdus531@naver.com>
2022-11-15 02:19:53 +0900

    헤더 CSS 수정 StockList CSS 수정

---
+++ src/main/java/gachon/jupoza/controller/MainController.java
@@ -0,0 +1,47 @@
+package gachon.jupoza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/index")
+    public String index()
+    {
+        return "index.html";
+    }
+    @GetMapping("/header")
+    public String header()
+    {
+        return "header.html";
+    }
+    @GetMapping("/button")
+    public String button()
+    {
+        return "buttons.html";
+    }
+    @GetMapping("/portfolio")
+    public String portfolio()
+    {
+        return "portfolio.html";
+    }
+    @GetMapping("/blank")
+    public String blank()
+    {
+        return "blank.html";
+    }
+    @GetMapping("/blank2")
+    public String blank2()
+    {
+        return "blank2.html";
+    }
+    @GetMapping("/blank3")
+    public String blank3()
+    {
+        return "blank3.html";
+    }
+
+
+
+
+}
