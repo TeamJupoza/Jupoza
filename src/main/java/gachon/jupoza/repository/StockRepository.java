34fcb566c9a69967280ad69403f0ab5360be9a57
yuminsang <nbghza@gmail.com>
2023-03-07 01:37:46 +0900

    #28 - Repository 추가
    erd를 참고해 필요한 엔티티의 repository를 추가함

--- src/main/java/gachon/jupoza/repository/StockRepository.java
+++ src/main/java/gachon/jupoza/repository/StockRepository.java
@@ -3,11 +3,9 @@ package gachon.jupoza.repository;
 import gachon.jupoza.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface StockRepository extends JpaRepository<Stock, Integer> {
     Stock findByname(String name);
 
