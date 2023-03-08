dd8a59204807af669a0e824ce29e9a39bb706c1d
yuminsang <nbghza@gmail.com>
2023-03-08 16:27:32 +0900

    #31 - 컨트롤러 추가
    메인으로 체크아웃이 안되서 여기서 메인으로 푸쉬함

---
+++ src/main/java/gachon/jupoza/controller/MyPortfolioController.java
@@ -0,0 +1,17 @@
+package gachon.jupoza.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/myportfolio")
+public class MyPortfolioController {
+
+    // 내 DB에 포트폴리오 정보를 저장
+
+    // 내 포트폴리오 정보 출력
+
+    // 내 포트폴리오 정보 삭제
+
+    // 내 포트폴리오 정보 수정
+}
