ff2e17226a41b938c04cce32b5021f0653b6404d
yuminsang <nbghza@gmail.com>
2023-03-08 16:12:31 +0900

    #31 -  엔티티의 Dto 수정및 삭제
    필요한 엔티티의 Dto 를 수정하고 추가 삭제함

--- src/main/java/gachon/jupoza/dto/PortfolioDTO.java
+++ src/main/java/gachon/jupoza/dto/PortfolioDTO.java
@@ -1,12 +1,21 @@
 package gachon.jupoza.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import gachon.jupoza.domain.Stock;
+import gachon.jupoza.domain.UserAccount;
+import gachon.jupoza.domain.Weights;
+import lombok.Data;
 
-@Getter
-@NoArgsConstructor
-public class PortfolioDTO {
-    String stock1;
-    String name;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * A DTO for the {@link gachon.jupoza.domain.PortFolio} entity
+ */
+@Data
+public class PortFolioDto {
+    private final Long id;
+    private final UserAccount userAccount;
+    private final List<Stock> StockList;
+    private final Weights weights;
 
 }
--- src/main/java/gachon/jupoza/dto/StockDTO.java
+++ src/main/java/gachon/jupoza/dto/StockDTO.java
@@ -1,52 +1,28 @@
 package gachon.jupoza.dto;
 
-import gachon.jupoza.domain.Stock;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
-@Getter
-@NoArgsConstructor
-public class StockDTO {
+import java.io.Serializable;
 
-    private int no;
-    private int stockId;
-    private String name;
-    private int price;
-    private float per;
-    private float roe;
-    private float dividend;  //매출
-    private float rate;  //연간주식상승률
-    private float net; //순이익 상승률
-    private float bis;  //자기자본비율
-    private float current;  //유동비율
-    private float quick;  //당좌비율
-    private float debt;  //부채비율
-    private float score;  //계산점수
-    private float sale;  //배당률
-    private float std;  //표준편차
-    private String img;
-
-
-    @Builder
-    public StockDTO (int no, Stock entity){
-        this.no = no;
-        this.stockId = entity.getStockId();
-        this.name = entity.getName();
-        this.price = entity.getPrice();
-        this.per = entity.getPer();
-        this.roe = entity.getRoe();
-        this.dividend = entity.getDividend();
-        this.rate = entity.getRate();
-        this.net = entity.getNet();
-        this.bis = entity.getBis();
-        this.current = entity.getCurrent();
-        this.quick = entity.getQuick();
-        this.debt = entity.getDebt();
-        this.score = entity.getScore();
-        this.sale = entity.getSale();
-        this.std = entity.getStd();
-        this.img = entity.getImg();
-    }
-
+/**
+ * A DTO for the {@link gachon.jupoza.domain.Stock} entity
+ */
+@Data
+public class StockDto implements Serializable {
+    private final int stockId;
+    private final String name;
+    private final int price;
+    private final float per;
+    private final float roe;
+    private final float dividend;
+    private final float rate;
+    private final float net;
+    private final float bis;
+    private final float current;
+    private final float quick;
+    private final float debt;
+    private final float score;
+    private final float sale;
+    private final float std;
+    private final String img;
 }
