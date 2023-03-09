b37acf8d7d106c625790d8a8c39cb86be2ccf6fc
yuminsang <nbghza@gmail.com>
2023-03-09 15:24:09 +0900

    #30 - 테스트 코드 작성시 도메인 수정
    1. PortFolio 클래스의 StockList 를 ArrayList -> List로 변경
    2. PortFolioDto에 from 메서드를 static 으로 변경 , ArrayList 캐스팅 삭제
    3. Weights 클래스의 from 메서드를 static 으로 변경
    4. UserAccountRepository 에 userid 로  계정을 찾을수 있는 메서드 추가

--- src/main/java/gachon/jupoza/repository/UserAccountRepository.java
+++ src/main/java/gachon/jupoza/repository/UserAccountRepository.java
@@ -4,4 +4,5 @@ import gachon.jupoza.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    UserAccount findByUserId(String UserId);
 }
