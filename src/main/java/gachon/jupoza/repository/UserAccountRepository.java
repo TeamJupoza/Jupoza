8e73c4baa0ede75435fb74708cc81b617b4921f2
yuminsang <nbghza@gmail.com>
2023-03-09 23:05:34 +0900

    #30 - 빌드시 에러 잡음
    UserAccount 에 잘못된 도메인 내용을 고쳤다.

--- src/main/java/gachon/jupoza/repository/UserAccountRepository.java
+++ src/main/java/gachon/jupoza/repository/UserAccountRepository.java
@@ -3,6 +3,6 @@ package gachon.jupoza.repository;
 import gachon.jupoza.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    UserAccount findByUserId(String UserId);
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+//    UserAccount findByUserId(String UserId);
 }
