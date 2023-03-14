621aa3e1e2a5e24258af521d23774ad8804a5b67
yuminsang <nbghza@gmail.com>
2023-03-14 14:57:37 +0900

    #39 - Repository 수정
    Reopository에 필요한 쿼리를 추가함

--- src/main/java/gachon/jupoza/repository/PortfolioRepository.java
+++ src/main/java/gachon/jupoza/repository/PortfolioRepository.java
@@ -1,9 +1,16 @@
 package gachon.jupoza.repository;
 
+
 import gachon.jupoza.domain.PortFolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface PortfolioRepository extends JpaRepository<PortFolio, Long> {
+
+     PortFolio findByUserAccount_UserId(String userId);
+
+     void deleteByUserAccount_UserId(String userId);
 
 
 }
