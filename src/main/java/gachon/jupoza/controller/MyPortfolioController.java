5ce7a4b7e1895511b235973b1ace9d8d7ad83367
yuminsang <nbghza@gmail.com>
2023-03-14 15:01:21 +0900

    #39 - 컨트롤러, 서비스 코드 추가
    PortFolio를 조회,수정,삭제,추가를 할 수있도록 컨트롤러와 서비스 코드를 추가함.
    
    
    ` JPA에서 연관된 엔티티를 삭제하기 위해서 CasCade로 부모 객체를 삭제하면 자동으로 삭제될 줄 알았는데 자동으로 삭제되지 않고 수동으로 연결을 null 처리 한 후에 삭제를 해야 삭제가 수행됨 `
    
    `엔티티를 업데이트를 할떄 update 쿼리가 나와서 update 가 될 줄 알았는데 엔티티를 삭제한후 추가하는 형식으로 동작함 `
    
    
    JPA에 대한 공부가 부족해서 내가 야매로 한거같음

--- src/main/java/gachon/jupoza/controller/MyPortfolioController.java
+++ src/main/java/gachon/jupoza/controller/MyPortfolioController.java
@@ -1,24 +1,20 @@
 package gachon.jupoza.controller;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import gachon.jupoza.domain.PortFolio;
 import gachon.jupoza.domain.Stock;
 import gachon.jupoza.dto.PortFolioDto;
 import gachon.jupoza.dto.Request.PortfolioRequest;
 import gachon.jupoza.dto.UserAccountDto;
+import gachon.jupoza.repository.StockRepository;
 import gachon.jupoza.service.MyPortfolioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -28,6 +24,8 @@ import java.util.*;
 public class MyPortfolioController {
 
     private final MyPortfolioService myPortfolioService;
+
+    private final StockRepository stockRepository;
 
     // 내 DB에 포트폴리오 정보를 저장
     @PostMapping("/save")
@@ -50,8 +48,72 @@ public class MyPortfolioController {
     }
 
     // 내 포트폴리오 정보 출력
+    @PostMapping("/print")
+    public Map<String,Object> GetMyPortFolio(@RequestBody Map<String,Object> request) throws JsonProcessingException {
+        log.info("{}",request);
 
-    // 내 포트폴리오 정보 삭제
+        String userId = String.valueOf(request.get("userId"));
+
+        // userId 를 이용해서 내 포트폴리오 정보를 가져옴
+        PortFolio portFolio = myPortfolioService.getPortfolio(userId);
+        log.info(portFolio.toString());
+
+
+        // 내 포트폴리오 정보를 이용해 stock들의 정보를 가져옴
+        List<Stock> stockList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            stockList.add(myPortfolioService.getStock(portFolio.getMyStockList().get(i).getStockId()));
+
+        }
+        log.info(stockList.toString());
+
+        // Map을 통해 출력한 결과를 클라이언트로 전송
+        Map<String,Object> result = new HashMap<>();
+        result.put("result","success");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+
+        result.put("portFolio",objectMapper.writeValueAsString(portFolio));
+        result.put("stockList",objectMapper.writeValueAsString(stockList));
+
+        System.out.println(result);
+        return result;
+
+    }
+
 
     // 내 포트폴리오 정보 수정
+    @PostMapping("/update")
+    public Map<String,Object> GetMyPortFolio( @RequestBody PortfolioRequest portfolioRequest) throws JsonProcessingException {
+
+        UserAccountDto userAccountDto = UserAccountDto.of("minsang","minsang","minsang@naver.com","GangHal","memo");
+        PortFolioDto portFolioDto = portfolioRequest.toDto(userAccountDto);
+
+        myPortfolioService.updatePortfolio(userAccountDto.getUserId(), portFolioDto);
+
+
+        Map<String,Object> result = new HashMap<>();
+        result.put("result","success");
+
+        return result;
+
+    }
+
+    
+    // 내 포트폴리오 정보 삭제
+    @PostMapping("/delete")
+
+    public Map<String,Object> deleteMyPortFolio(@RequestBody Map<String,Object> request)
+    {
+        String userId = String.valueOf(request.get("userId"));
+
+        myPortfolioService.DeletePortFolio(userId);
+
+        Map<String,Object> result = new HashMap<>();
+        result.put("result","success");
+
+        return result;
+
+    }
 }
--- src/main/java/gachon/jupoza/service/MyPortfolioService.java
+++ src/main/java/gachon/jupoza/service/MyPortfolioService.java
@@ -1,9 +1,13 @@
 package gachon.jupoza.service;
 
+import gachon.jupoza.domain.MyStock;
 import gachon.jupoza.domain.PortFolio;
+import gachon.jupoza.domain.Stock;
 import gachon.jupoza.domain.UserAccount;
 import gachon.jupoza.dto.PortFolioDto;
+import gachon.jupoza.repository.MyStockRepository;
 import gachon.jupoza.repository.PortfolioRepository;
+import gachon.jupoza.repository.StockRepository;
 import gachon.jupoza.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +24,15 @@ public class MyPortfolioService {
 
      private final PortfolioRepository portfolioRepository;
 
+     private final StockRepository stockRepository;
+
      private final UserAccountRepository userAccountRepository;
+
+     private final MyStockRepository myStockRepository;
+
+
+
+
 
     // Portfolio Dto를 UserAccount 정보와 같이 엔티티화 해서 DB에 저장한다.
     public void savePortfolio(PortFolioDto dto) {
@@ -30,27 +42,64 @@ public class MyPortfolioService {
         // MyPortfolio 엔티티에 포트폴리오 정보를 매핑하기 위해 넣어줌
         for (int i = 0; i < 5; i++) {
             portFolio.getMyStockList().get(i).setPortFolio(portFolio);
+            portFolio.getMyStockList().get(i).setUserId(dto.getUserAccountDto().getUserId());
         }
 
         portfolioRepository.save(portFolio);
 
     }
 
-    public void updatePortfolio(Long id, PortFolioDto dto) {
-        try {
-            PortFolio portFolio = portfolioRepository.getReferenceById(id);
 
-            
-            UserAccount userAccount = userAccountRepository.getReferenceById(dto.getUserAccountDto().getUserId());
-            System.out.println(userAccount);
-            // 유저 정보가 입력되지안흥ㅁ
-//            if (portFolio.getUserAccount().getUser_id().equals(userAccount.getUser_id()))
-//            {
-//                if (dto.getStockList() != null){portFolio.setStockList(dto.getStockList());}
-//                if (dto.getWeights() != null){portFolio.setWeights(dto.getWeights());}
-//            }
-//            portFolio.setStockList(dto.getStockList());
-//            portFolio.setWeights(dto.getWeights());
+
+    /*
+       포트폴리오 업데이트
+       sql처럼 portfolio레코드를 삭제하면 cascade되어 자동으로 연관된 myStock레코드 까지 삭제될줄 알았는데
+       수동으로 연결을 끊어줘야 삭제가 가능함
+       내가 잘못한거 같은데 왜 안되는지 모르겠음
+
+     */
+    @Transactional
+    public void updatePortfolio(String userId, PortFolioDto portFolioDto) {
+        try {
+            // portfolio 정보, user 정보를 db에서 가져온다.
+            PortFolio portFolio = portfolioRepository.findByUserAccount_UserId(userId);
+            UserAccount userAccount = userAccountRepository.getReferenceById(portFolioDto.getUserAccountDto().getUserId());
+
+            // 유저 정보가 입력되지않음
+            if (portFolio.getUserAccount().getUserId().equals(userAccount.getUserId()))
+            {
+                if (portFolioDto.getStockList() != null)
+                {
+
+                    for (int i = 0; i < 5; i++) {portFolioDto.getStockList().get(i).setPortFolio(portFolio);}
+
+                    // myStock테이블과 PortFolio 테이블에 서로 연관을 null 처리함
+                    for ( MyStock mystock: portFolio.getMyStockList())
+                    {
+                        mystock.setPortFolio(null);
+                    }
+                    portFolio.setMyStockList(null);
+
+
+                    // portFolio, MyStock테이블의 수정할 정보를 먼저 삭제함
+                    portfolioRepository.deleteById(portFolio.getId());
+                    myStockRepository.deleteByUserId(portFolio.getUserAccount().getUserId());
+                    portfolioRepository.flush();
+
+                    // Dto 로 부터 수정할 정보를 PortFolio 엔티티로 변환
+                    PortFolio updatedPortFolio = portFolioDto.toEntity(userAccount);
+
+                    // 수정할 엔티티에 연관관계 설정
+                    for (int i = 0; i < 5; i++) {
+                        updatedPortFolio.getMyStockList().get(i).setPortFolio(updatedPortFolio);
+                        updatedPortFolio.getMyStockList().get(i).setUserId(portFolioDto.getUserAccountDto().getUserId());
+                    }
+
+                    // 수정할 엔티티를 테이블에 저장한다.
+                    portfolioRepository.save(updatedPortFolio);
+                }
+            }
+
         }catch (EntityNotFoundException e)
         {
             log.warn("포트폴리오 업데이트 실패. 포트폴리오를 업데이트 하는데 필요한 정보를 찾을수 없습니다. - {}", e.getLocalizedMessage());
@@ -58,12 +107,53 @@ public class MyPortfolioService {
 
     }
 
+    // 포트폴리오 출력
     @Transactional(readOnly = true)
-    public PortFolioDto getPortfolio(Long portfolioId) {
-        return portfolioRepository.findById(portfolioId)
-                .map(PortFolioDto::from)
-                .orElseThrow(() -> new EntityNotFoundException("포트폴리오가 없습니다. - articleId :" + portfolioId));
+    public PortFolio getPortfolio(String userId) {
+        try{
+            return portfolioRepository.findByUserAccount_UserId(userId);
+        }catch (Exception e)
+        {
+            e.getLocalizedMessage();
+            return null;
+        }
+
     }
+
+    // 유저아이디를 받아와 포트폴리오를 삭제한다.
+    // 포트폴리오엔티티와 MyStock엔티티를 둘다 삭제해야한다.
+    public void DeletePortFolio(String userId) {
+        PortFolio portFolio = portfolioRepository.findByUserAccount_UserId(userId);
+        UserAccount userAccount = userAccountRepository.getReferenceById(userId);
+
+        // 유저 정보가 입력되지않음
+        if (portFolio.getUserAccount().getUserId().equals(userAccount.getUserId())) {
+            if (portFolio.getMyStockList() != null) {
+
+                // myStock테이블과 PortFolio 테이블에 서로 연관을 null 처리함
+                for (MyStock mystock : portFolio.getMyStockList()) {
+                    mystock.setPortFolio(null);
+                }
+                portFolio.setMyStockList(null);
+
+
+                // portFolio, MyStock테이블의 수정할 정보를 먼저 삭제함
+                portfolioRepository.deleteById(portFolio.getId());
+                myStockRepository.deleteByUserId(portFolio.getUserAccount().getUserId());
+                portfolioRepository.flush();
+
+
+            }
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Stock getStock(int stockId)
+    {
+        return stockRepository.findById(stockId).orElseThrow(() -> new EntityNotFoundException("StockId에 해당하는 Stock이 없습니다. stockId : "+ stockId));
+
+    }
+
 
     public void deletePortfolio(Long id) {
         portfolioRepository.deleteById(id);
