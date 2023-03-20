45dccd33b9b370df322a24d12b2dccc08607ccf7
yuminsang <nbghza@gmail.com>
2023-03-20 14:11:29 +0900

    #44- MainController 수정
    articleDeatil 에 들어갈떄 articleId변수를 가지고 들어가기 위해 Pathvariable과 Modelmap 추가 
    
    article-detail 뷰를 리턴하는 메서드 추ㄱ

--- src/main/java/gachon/jupoza/controller/MainController.java
+++ src/main/java/gachon/jupoza/controller/MainController.java
@@ -1,60 +1,77 @@
 package gachon.jupoza.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Slf4j
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index()
-    {
+    public String index() {
         return "index.html";
     }
+
     @GetMapping("/header")
-    public String header()
-    {
+    public String header() {
         return "header.html";
     }
+
     @GetMapping("/button")
-    public String button()
-    {
+    public String button() {
         return "buttons.html";
     }
+
     @GetMapping("/portfolio")
-    public String portfolio()
-    {
+    public String portfolio() {
         return "portfolio.html";
     }
+
     @GetMapping("/blank")
-    public String blank()
-    {
+    public String blank() {
         return "blank.html";
     }
+
     @GetMapping("/blank2")
-    public String blank2()
-    {
+    public String blank2() {
         return "blank2.html";
     }
+
     @GetMapping("/blank3")
-    public String blank3()
-    {
+    public String blank3() {
         return "blank3.html";
     }
+
     @GetMapping("/category")
-    public String category()
-    {
+    public String category() {
         return "category.html";
     }
 
     @GetMapping("/myportfolio")
-    public String myPortFolio() {return "my-portfolio.html";}
+    public String myPortFolio() {
+        return "my-portfolio.html";
+    }
 
     @GetMapping("/articleform")
-    public String articleForm(){return "article-form.html";}
+    public String articleForm() {
+        return "article-form.html";
+    }
 
-    @GetMapping("/articledetail")
-    public String articleDetail(){return "article-detail.html";}
+    @GetMapping("/articledetail/{articleId}")
+    public String articleDetail(@PathVariable String articleId, ModelMap map)
+    {
 
+        map.addAttribute("articleId", articleId);
+        return "article-detail.html";
+    }
+
+    @GetMapping("/article")
+    public String articles() {
+        return "article.html";
+
+    }
 
 
 }
