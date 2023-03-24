d167b8bb99f7e9b6ea22a3839702946e5cb5e0bf
yuminsang <nbghza@gmail.com>
2023-03-24 16:15:59 +0900

    #53 - security config 생성
    jwt 필터를 생성하고 security 를 생성함
    
    로그인시 뷰에서 인증이 필요한 부분을 없애거나 보이는 식으로 처리해서 인증과정을 없앰

--- src/main/java/gachon/jupoza/config/SecurityConfig.java
+++ src/main/java/gachon/jupoza/config/SecurityConfig.java
@@ -1,18 +1,53 @@
 package gachon.jupoza.config;
 
+import gachon.jupoza.jwt.JwtAuthenticationFilter;
+import gachon.jupoza.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin()
-                .and().csrf().disable()
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http
+                // rest api 이므로 basic auth 및 csrf 보안을 사용하지 않는다
+                .httpBasic().disable()
+                .csrf().disable()
+                // Jwt를 사용하기 떄문에 세션은 사용하지않는다.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 해당 api에 대해 모든 요청을 허용한다.
+//                .antMatchers("/myportfolio").authenticated()
+//                .antMatchers("/articleform").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                // jwt 인증을 위하여 직접 구현한 필터를 usernamepasswordAuthenticationFilter 전에 실행하겠다는 의미
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    // Jwt 를 사용하기 위해 기본적으로 Password ewncoder 가 필요한데 여기서는 Bycrypt encoder 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
+
+
+
