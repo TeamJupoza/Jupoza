3b491bfbb81693da31fc5ceaa70d6451c1e7b8a2
yuminsang <nbghza@gmail.com>
2023-03-27 16:01:27 +0900

    포트폴리오 뷰에 주식 5개를 가져올떄 버그픽스
    로직수정함

--- src/main/resources/static/js/portfolio.js
+++ src/main/resources/static/js/portfolio.js
@@ -11,12 +11,14 @@ let ResponseWeight = []
 
 // portfolio 접속시 화면 페이지 구성 함수 실행
 $(document).ready(function () {
-    if (localStorage.length != 5)
+
+
+    if (JSON.parse(localStorage.getItem("items")).length != 5)
     {
-        alert('먼저 주식을 담아주세요.')
+        alert('먼저 주식 5개를 담아주세요.')
         location.replace("/list")
     }
-    //RequestStocks = ["삼성전자", "LG", "포스코케미칼", "현대차", "삼성중공업"]
+
     // 로컬스토리지에서 포트폴리오 변수들을 가져옴
     let storage = []
     storage = JSON.parse(localStorage.getItem('items'))
